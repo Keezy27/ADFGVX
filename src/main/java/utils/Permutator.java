912a7ca26242e80dbce5482dc62db60ea002ee0a
keezy27 <inf.carmine@gmail.com>
2019-05-12 12:51:54 +0200

    Permutator - generateUnorderedMatrice created to generate the matrice with the key as first line and the message with 1st cycle encrypted

--- src/main/java/utils/Permutator.java
+++ src/main/java/utils/Permutator.java
@@ -23,6 +23,38 @@ public class Permutator {
         this.key = key;
     }
 
+    public void generateUnorderedMatrice(String key, String encryptedMessage){
+        char[] keyArray = key.toCharArray();
+        char[] encryptedArray = encryptedMessage.toCharArray();
+        char[][] unorderedMatrice = new char[getNumberOfRow(key,encryptedMessage)][key.length()];
+
+        int messageIndex = 0;
+        for(int i = 0; i < getNumberOfRow(key,encryptedMessage); i++){
+            for(int j = 0; j < keyArray.length; j++){
+                if(i == 0){
+                    unorderedMatrice[i][j] = keyArray[j];
+                }else{
+                    if(messageIndex < encryptedArray.length){
+                        unorderedMatrice[i][j] = encryptedArray[messageIndex];
+                        messageIndex++;
+                    }else{
+                        unorderedMatrice[i][j] = ' ';
+                    }
+                }
+            }
+        }
+
+        /*for(int i = 0; i < getNumberOfRow(key,encryptedMessage); i++) {
+            for (int j = 0; j < keyArray.length; j++) {
+                System.out.print("| " + unorderedMatrice[i][j] + " |");
+                if( j == (keyArray.length -1)){
+                    System.out.println("\n---------------------------------");
+                    System.out.println();
+                }
+            }
+        }*/
+    }
+
     private int getNumberOfRow(String key, String encryptedMessage){
         int nbRow;
         int keyLength = key.length();
